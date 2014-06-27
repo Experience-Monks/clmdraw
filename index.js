25d946ce748c68505442843c61731307d544f435
Mikko Haapoja <me@mikkoh.com>
2014-06-26 20:36:02 -0400

    You've been had by the snake

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+	draw: require( './lib/draw' ),
+	drawForehead: require( './lib/drawForehead' ),
+	drawJaw: require( './lib/drawJaw' ),
+	drawLeftEye: require( './lib/drawLeftEye' ),
+	drawLeftEyeBrow: require( './lib/drawLeftEyeBrow' ),
+	drawLips: require( './lib/drawLips' ),
+	drawNose: require( './lib/drawNose' ),
+	drawRightEye: require( './lib/drawRightEye' ),
+	drawRightEyeBrow: require( './lib/drawRightEyeBrow' )
+};
